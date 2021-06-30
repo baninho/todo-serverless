57d7092950088cefc36625e8f5252c00c3f6bd24
baninho <florianimbt@gmail.com>
2021-06-30 18:28:25 -0500

    feature: remove getUploadUrl from createTodo and permissions

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -9,13 +9,9 @@ import { TodoItem } from '../../models/TodoItem'
 import { getUserId } from '../utils'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
-const s3 = new AWS.S3({
-  signatureVersion: 'v4'
-})
 
 const todoTable = process.env.TODO_TABLE
 const bucketName = process.env.ATTACHMENTS_BUCKET
-const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
@@ -39,21 +35,10 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     Item: todoItem
   }).promise()
 
-  const uploadUrl = getUploadUrl(todoId)
-
   return {
     statusCode: 201,
     body: JSON.stringify({
       newItem: todoItem,
-      uploadUrl: uploadUrl
     })
   }
-}
-
-function getUploadUrl(todoId: string) {
-  return s3.getSignedUrl('putObject', {
-    Bucket: bucketName,
-    Key: todoId,
-    Expires: parseInt(urlExpiration)
-  })
 }
