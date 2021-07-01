5109bd4b28052a8e7976f0150ea74c1dc451fa7e
baninho <florianimbt@gmail.com>
2021-07-01 14:35:20 -0500

    feature: add logging

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -6,10 +6,15 @@ import { cors } from 'middy/middlewares'
 
 import { getUserId } from '../utils'
 import { getTodosByUser } from '../../main/todos'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('getTodos')
 
 export const handler = middy( async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // Get all TODO items for a current user
   const userId = getUserId(event)
+
+  logger.info(`getting todos for user ${userId}`)
 
   const items = await getTodosByUser(userId)
 
