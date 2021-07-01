146f9e979a7710e140b982f16029e7c82d96e9d6
baninho <florianimbt@gmail.com>
2021-06-30 20:20:31 -0500

    feature: add logging

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -5,8 +5,10 @@ import * as AWS from 'aws-sdk'
 import { getUserId } from '../utils'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
+import { createLogger } from '../../utils/logger'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
+const logger = createLogger('deleteTodo')
 
 const todoTable = process.env.TODO_TABLE
 const todoIdIndex = process.env.TODO_ID_INDEX
@@ -34,6 +36,8 @@ export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGat
     }
   }
 
+  logger.info(`delete params: ${params}`)
+  
   await docClient.delete(params).promise()
 
   return {
