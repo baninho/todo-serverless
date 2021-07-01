70b933b0ef7cf5be3b8c00654ec0de1bd4ce726e
baninho <florianimbt@gmail.com>
2021-07-01 14:17:35 -0500

    feature: add logging

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -4,16 +4,20 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as AWS  from 'aws-sdk'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
+import { createLogger } from '../../utils/logger'
 
 const s3 = new AWS.S3({
   signatureVersion: 'v4'
 })
+const logger = createLogger('uploadUrl')
 
 const bucketName = process.env.ATTACHMENTS_BUCKET
 const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+
+  logger.info(`Upload URL for todo ${todoId} requested`)
 
   const uploadUrl = s3.getSignedUrl('putObject', {
     Bucket: bucketName,
