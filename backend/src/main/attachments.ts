096391e6d6be3c435ff1b92c7d222d5ce60a6ed8
baninho <florianimbt@gmail.com>
2021-07-03 12:22:48 -0500

    refactor: move deleteObject to S3Access move call to deleteObject to request handler deleteTodo

--- backend/src/main/attachments.ts
+++ backend/src/main/attachments.ts
@@ -5,3 +5,7 @@ const s3access = new S3Access()
 export async function getUploadUrl(todoId: string): Promise<string> {
   return s3access.getUploadUrl(todoId)
 }
+
+export async function deleteObject(todoId: string) {
+  await s3access.deleteObject(todoId)
+}
